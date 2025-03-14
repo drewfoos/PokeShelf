74c21e70cf4d9c428ddf35ad96e2f196df6932f8
drewfoos <drewfoos@yahoo.com>
2025-03-14 11:30:15 -0400

    added next sitemap config

---
+++ next-sitemap.config.ts
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.pokeshelf.com',
+    generateRobotsTxt: true,
+    // You can adjust additional options as needed.
+  };
+  
