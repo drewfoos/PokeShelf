fc3fd90e991f9379fe12bad10e46eee3768c274d
drewfoos <drewfoos@yahoo.com>
2025-03-14 12:28:14 -0400

    removed sitemap app/

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,11 @@
 // next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://www.pokeshelf.com',
   generateRobotsTxt: true,
-  additionalPaths: async () => { // Remove unused config parameter
-    const prisma = require('./lib/prisma').default;
+  additionalPaths: async () => {
+    const prisma = require(`${process.cwd()}/lib/prisma`).default;
+    // Fetch all sets and cards from your database
     const sets = await prisma.set.findMany({ select: { id: true } });
     const cards = await prisma.card.findMany({ select: { id: true } });
     
