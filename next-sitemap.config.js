ba073e6ade029c7e2eb18f384a108e10f2ae0ba2
drewfoos <drewfoos@yahoo.com>
2025-03-14 12:15:52 -0400

    removed conflicting sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,15 @@
-/** @type {import('next-sitemap').IConfig} */
+// next-sitemap.config.js
 module.exports = {
-    siteUrl: 'https://www.pokeshelf.com',
-    generateRobotsTxt: true,
-    // You can adjust additional options as needed.
-  };
-  
+  siteUrl: 'https://www.pokeshelf.com',
+  generateRobotsTxt: true,
+  additionalPaths: async () => { // Remove unused config parameter
+    const prisma = require('./lib/prisma').default;
+    const sets = await prisma.set.findMany({ select: { id: true } });
+    const cards = await prisma.card.findMany({ select: { id: true } });
+    
+    return [
+      ...sets.map(set => `/sets/${set.id}`),
+      ...cards.map(card => `/card/${card.id}`)
+    ];
+  },
+};
